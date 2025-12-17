e8b5f45cca3e9dfe97c4289b6f7da6a09045d383
Peraspera1 <3210103812@zju.edu.cn>
2025-12-17 12:32:34 +0800

    update 12.17 2

--- legged_gym/envs/go2/go2_env.py
+++ legged_gym/envs/go2/go2_env.py
@@ -65,15 +65,17 @@ class go2Robot(LeggedRobot):
 
     def _reward_tracking_pos(self):
         x_error = torch.square(self.base_pos[:,0]-3)
+        y_error1 = torch.square(self.base_pos[:,1])
         y_error = torch.square(self.base_pos[:,1]-3)
-        reward_phase_1 = torch.exp(-x_error/self.cfg.rewards.tracking_sigma)
+        reward_phase_1 = torch.exp(-x_error/self.cfg.rewards.tracking_sigma) - y_error1
         reward_phase_2 = torch.where(x_error<0.5, torch.exp(-y_error/self.cfg.rewards.tracking_sigma),torch.zeros_like(y_error))
         reward = reward_phase_1 + reward_phase_2
-        # print(reward_phase_1,reward_phase_2)
+        print(reward_phase_1,reward_phase_2,torch.exp(-x_error/self.cfg.rewards.tracking_sigma),y_error1)
         return reward
     
-    # def _reward_tracking_lin_vel(self):
+    # def _reward_heading(self):
     #     desired_pos1 = torch.tensor([3,0],device=self.device)
     #     desired_pos2 = torch.tensor([3,3],device=self.device)
+
 
     
